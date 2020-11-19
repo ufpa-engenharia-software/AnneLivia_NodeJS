4ccbaab1b6ef68ba002f9f04feafff22758c6abc
Anne Livia <annelivia16@gmail.com>
2020-11-19 19:30:35 +0000

    updated a few things

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,33 +1,33 @@
 const bodyParser = require('body-parser')
 module.exports = app => {
-	const Usuarios = app.models.usuarios;
-	app.use(bodyParser.urlencoded({ extended: true }));
+  const Usuarios = app.models.usuarios;
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-	app.get("/usuarios", (req, res) => {
-			Usuarios.findAll({},(retorno)=>{res.json({usuarios: retorno})});
-	});
+  app.get("/usuarios", (req, res) => {
+    Usuarios.findAll({}, (retorno) => { res.json({ usuarios: retorno }) });
+  });
 
   // create application/json parser
   var jsonParser = bodyParser.json()
-	app.post("/usuarios", jsonParser, function(req, res) {
-		
-			var nome = req.body.nome;
- 			var idUsuario = req.body.id;
-      var email = req.body.email;
-      var dataNascimento = req.body.dataNascimento;
-      var login = req.body.login;
-      var senha = req.body.senha;
+  app.post("/usuarios", jsonParser, function(req, res) {
 
-      console.log(req.body);
-			res.json(
-				{
-					nomeRecebido: nome,
-					idRecebido: idUsuario,
-          emailRecebido: email,
-          dataNascimentoRecebido: dataNascimento,
-          loginRecebido: login,
-          senhaRecebido: senha,
-				}
-			)
-	});
+    var nome = req.body.nome;
+    var idUsuario = req.body.id;
+    var email = req.body.email;
+    var dataNascimento = req.body.dataNascimento;
+    var login = req.body.login;
+    var senha = req.body.senha;
+
+    console.log(req.body);
+    res.json(
+      {
+        nomeRecebido: nome,
+        idRecebido: idUsuario,
+        emailRecebido: email,
+        dataNascimentoRecebido: dataNascimento,
+        loginRecebido: login,
+        senhaRecebido: senha,
+      }
+    )
+  });
 };
